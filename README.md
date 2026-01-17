329b4481ad6945434c34331aa47c13573604fe8a
myd7349 <myd7349@gmail.com>
2026-01-17 12:05:25 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# C++ CI Playground
+
+C++ & CMake & GitHub Actions.
+
+# LICENSE
+
+MIT
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+// https://github.com/zeux/pugixml/blob/master/src/pugixml.hpp#L40-L46
+// Check if std::string_view is available
+#if !defined(PUGIXML_HAS_STRING_VIEW) && !defined(PUGIXML_NO_STL)
+#    if __cplusplus >= 201703L
+#        define PUGIXML_HAS_STRING_VIEW
+#    elif defined(_MSVC_LANG) && _MSVC_LANG >= 201703L
+#        define PUGIXML_HAS_STRING_VIEW
+#    endif
+#endif
+
+int main()
+{
+#ifdef PUGIXML_HAS_STRING_VIEW
+    std::cout << "PUGIXML_HAS_STRING_VIEW is defined." << std::endl;
+#else
+    std::cout << "PUGIXML_HAS_STRING_VIEW is not defined." << std::endl;
+#endif
+
+    return 0;
+}
